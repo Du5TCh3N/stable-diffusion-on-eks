e4f7aa77e5f1588f5becc03f6421880888e79fb9
Guangzhong Chen <guangzhong0@gmail.com>
2024-03-22 19:52:11 +0000

    30

--- lib/runtime/sdRuntime.ts
+++ lib/runtime/sdRuntime.ts
@@ -31,7 +31,7 @@ export const defaultProps: blueprints.addons.HelmAddOnProps & SDRuntimeAddOnProp
   namespace: 'sdruntime',
   release: 'sdruntime',
   version: '0.1.0',
-  repository: 'http://54.155.117.36:8099/',
+  repository: 'http://54.171.240.175:8099/',
   values: {
     global: {
       awsRegion: cdk.Aws.REGION,
