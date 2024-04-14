7aa18beae954b172670f5449746fe4fe8bc2cd86
Guangzhong Chen <guangzhong0@gmail.com>
2024-04-14 16:15:58 +0100

    inpaint api version 15

--- lib/runtime/sdRuntime.ts
+++ lib/runtime/sdRuntime.ts
@@ -31,7 +31,7 @@ export const defaultProps: blueprints.addons.HelmAddOnProps & SDRuntimeAddOnProp
   namespace: 'sdruntime',
   release: 'sdruntime',
   version: '0.1.0',
-  repository: 'http://54.194.112.128:8099/',
+  repository: 'http://34.244.236.4:8099/',
   values: {
     global: {
       awsRegion: cdk.Aws.REGION,
